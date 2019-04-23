90925d838ad0c40854d0f3150139b13552d5cdcd
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2019-04-23 20:12:21 +0700

    tambah js setting & ubah isi html setting - BELUM SELESAI

---
+++ src/Setting.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Setting extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			feedUrl: [],
+		}
+	}
+
+	render() {
+		return(
+			<div className="row">
+				<div className="col-md-12">
+					
+				</div>
+			</div>
+		)
+	}
+}
