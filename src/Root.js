dd1b073c28ea38f1eea70384d99a2e2732eecf24
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2019-06-26 13:25:25 +0700

    ubah root jadi komponen react

--- src/Root.js
+++ src/Root.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 import News from './News'
@@ -27,36 +27,42 @@ function RouteWithSubRoutes(route) {
 	);
 }
 
-function Root() {
-	return (
-		<Router>
-			<div className="row">
-				<div className="col-md-12 no-padding-right no-padding-left border-bottom">
-					<div className="btn-toolbar toolbar-padding" role="toolbar" aria-label="Button Toolbar">
-						<div className="btn-group mr-2" role="group" id="button-group">
-							<NavLink to="/News" className="btn btn-outline-primary btn-sm" id="newsButton" activeClassName="active">
-							<i className="fa fa-newspaper-o"></i> Feeds
-							</NavLink>
-							<NavLink to="/Setting" className="btn btn-outline-primary btn-sm" id="settingButton" activeClassName="active">
-							<i className="fa fa-wrench"></i>
-							</NavLink>
+class Root extends Component {
+	constructor(props) {
+		super(props)
+	}
+	
+	render() {
+		return (
+			<Router>
+				<div className="row">
+					<div className="col-md-12 no-padding-right no-padding-left border-bottom">
+						<div className="btn-toolbar toolbar-padding" role="toolbar" aria-label="Button Toolbar">
+							<div className="btn-group mr-2" role="group" id="button-group">
+								<NavLink to="/News" className="btn btn-outline-primary btn-sm" id="newsButton" activeClassName="active">
+								<i className="fa fa-newspaper-o"></i> Feeds
+								</NavLink>
+								<NavLink to="/Setting" className="btn btn-outline-primary btn-sm" id="settingButton" activeClassName="active">
+								<i className="fa fa-wrench"></i>
+								</NavLink>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-
-			{ routes.map((route, i) => (
-				<RouteWithSubRoutes key={i} {...route} />
-			))}
-
-			<div className="row">
-				<div className="col-md-12" style={{ height: '100vh' }}>
-					<img src={img} alt="Logo" className="working-center-image" style={{ height: '512px', width: '512px' }}/>
+	
+				{ routes.map((route, i) => (
+					<RouteWithSubRoutes key={i} {...route} />
+				))}
+	
+				<div className="row">
+					<div className="col-md-12" style={{ height: '100vh' }}>
+						<img src={img} alt="Logo" className="working-center-image" style={{ height: '512px', width: '512px' }}/>
+					</div>
 				</div>
-			</div>
-		</Router>
-
-	);
+			</Router>
+	
+		);
+	}	
 }
 
 export default Root
