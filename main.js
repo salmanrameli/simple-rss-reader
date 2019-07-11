d6d88a7e1ddcc49a628c2216bf916f76a5aa0f79
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2019-07-11 13:34:42 +0700

    tambah mekanisme cek apakah auth code masih valid atau perlu login ulang

--- main.js
+++ main.js
@@ -36,11 +36,30 @@ function createWindow () {
 	})
 }
 
+function createLoginWindow() {
+	let window = new BrowserWindow({
+		width: 600, 
+		height: 800,
+	});
+
+	window.loadURL("https://cloud.feedly.com/v3/auth/auth?response_type=code&client_id=feedlydev&redirect_uri=http%3A%2F%2Flocalhost%3A8080&scope=https%3A%2F%2Fcloud.feedly.com%2Fsubscriptions")
+}
+
 function setBadge(num) {
 	let dock = app.dock
 
 	dock.setBadge('' + num)
 }
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+	if (process.platform !== 'darwin') app.quit()
+})
+  
+app.on('activate', function () {
+	if (win === null) createWindow()
+})
 
 ipcMain.on('asynchronous-message', (event, arg) => {
 	let integrationState = store.get('integrateWithFeedly')
@@ -74,25 +93,6 @@ ipcMain.on('unread-count', (event, arg) => {
 
 ipcMain.on('decrease-unread-count', (event, arg) => {
 	setBadge(unreadCount--)
-})
-
-function createLoginWindow() {
-	let window = new BrowserWindow({
-		width: 600, 
-		height: 800,
-	});
-
-	window.loadURL("https://cloud.feedly.com/v3/auth/auth?response_type=code&client_id=feedlydev&redirect_uri=http%3A%2F%2Flocalhost%3A8080&scope=https%3A%2F%2Fcloud.feedly.com%2Fsubscriptions")
-}
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', function () {
-	if (process.platform !== 'darwin') app.quit()
-})
-  
-app.on('activate', function () {
-	if (win === null) createWindow()
 })
 
 let menubar = [
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import Axios from 'axios';
 import Root from './Root';
 import FeedlyRoot from './Feedly/FeedlyRoot'
 import Login from './Login'
+import { getAuthCode } from './Feedly/UserDetails'
+import { getProfile } from './Feedly/Constants'
 
 const { ipcRenderer } = window.require('electron')
 
@@ -22,7 +25,23 @@ ipcRenderer.on('asynchronous-reply', (event, reply) => {
 			render(Root)
 			break
 		case 'integrated':
-			render(FeedlyRoot)
+			const authCode = getAuthCode()
+			
+			Axios({
+				method: 'get',
+				url: getProfile(),
+				responseType: 'application/json',
+				headers: {
+					'Authorization': `OAuth ${authCode}`
+				}
+			}).then((response) => {
+				if(response.status === 200)
+					render(FeedlyRoot)
+				else
+					render(Login)
+			}).catch(function(error) {
+				console.log(error)
+			})
 			break
 		default:
 			render(Login)
